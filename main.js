de888e5b9966043e176a58744d29b0bdd41c9924
VarunKumar <varun.kumar.sharma.2365@gmail.com>
2025-04-19 21:33:01 +0530

    Mongo URL Updated

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.listen(PORT, () => {
   console.log(`Server Started At Port : ${PORT}`)
 })
 //Connecting the MongoDB Server
-const db = mongoose.connect('mongodb+srv://Varun:Varun9999@wifi-server.kvwhr.mongodb.net/?retryWrites=true&w=majority&appName=Wifi-Server/Wifi-Module', {
+const db = mongoose.connect('mongodb+srv://vercel-admin-user:Nf5OfOELwtG1rvlf@wifi-server.kvwhr.mongodb.net/Wifi-Module?retryWrites=true&w=majority', {
 
   dbName: 'Wifi-Module' // Ensure this is set.
 })
